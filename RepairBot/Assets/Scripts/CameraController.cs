1d739d318320b5ec91f564a88d26fb15ff92d84e
kyuu man <kyuuman@kyuus-MacBook-Pro.local>
2020-01-31 20:39:28 -0600

    added scripts

---
+++ RepairBot/Assets/Scripts/CameraController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private Transform player;
+
+    private float xAxisClamp;
+
+    private void Awake()
+    {
+        LockCursor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+        if (xAxisClamp > 90.0)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (xAxisClamp < -90.0)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        player.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
---
+++ RepairBot/Assets/Scripts/MovementController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rigid;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * moveSpeed) + (transform.right * Input.GetAxis("Horizontal") * moveSpeed));
+        if (Input.GetButtonDown("Jump") && transform.position.y <= 1.5)
+        {
+            rigid.AddForce(Vector3.up * jumpForce);
+        }
+    }
+}
