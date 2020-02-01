05f955ee7b0b228ab8f3d5b82aa422aaace976a8
Grant-Ross <36680394+Grant-Ross@users.noreply.github.com>
2020-02-01 12:59:30 -0600

    DONT PUSH

---
+++ RepairBot/Assets/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    
+    
+    private void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+        }
+    }
+
+    public void StartGameMusic()
+    {
+        Play("Head");
+    }
+    
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
