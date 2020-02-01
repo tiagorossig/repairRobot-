e6a568a84a081b3a1089d4812e026d06a2a806ea
kyuu man <kyuuman@kyuus-MacBook-Pro.local>
2020-02-01 08:33:39 -0600

    made sample scene plus parts adding code

---
+++ RepairBot/Assets/Scripts/Collecter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collecter : MonoBehaviour
+{
+
+    private bool haslegs;
+    private bool hasTorso;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        haslegs = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [System.Obsolete]
+    private void OnCollisionEnter(Collision collision)
+    { 
+        GameObject other = collision.gameObject;
+        if ("Torso".Equals(other.tag))
+        {
+            Destroy(other);
+            Vector3 temp = transform.position;
+            transform.position = new Vector3(temp.x, 1.6f, temp.z);
+            transform.FindChild("torso").gameObject.SetActive(true);
+            Vector3 cameratemp = transform.FindChild("Main Camera").position;
+            cameratemp.z = cameratemp.z - 2;
+            transform.FindChild("Main Camera").position = cameratemp;
+            hasTorso = true;
+        }
+        if (hasTorso)
+        {
+            if ("Arm".Equals(other.tag))
+            {
+                Destroy(other);
+                transform.FindChild("arm").gameObject.SetActive(true);
+            }
+            if ("Arm2".Equals(other.tag))
+            {
+                Destroy(other);
+                transform.FindChild("arm2").gameObject.SetActive(true);
+            }
+            if ("Leg".Equals(other.tag))
+            {
+                Destroy(other);
+                if (!haslegs)
+                {
+                    Vector3 temp = transform.position;
+                    transform.position = new Vector3(temp.x, 4f, temp.z);
+                    haslegs = true;
+                }
+                transform.FindChild("leg").gameObject.SetActive(true);
+            }
+            if ("Leg2".Equals(other.tag))
+            {
+                Destroy(other);
+                if (!haslegs)
+                {
+                    Vector3 temp = transform.position;
+                    transform.position = new Vector3(temp.x, 4f, temp.z);
+                    haslegs = true;
+                }
+                transform.FindChild("leg2").gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ RepairBot/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool isGrounded;
+    private float moveSpeed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) //check to see if the space bar is pressed && box is on ground
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 180); //add force to make the box jump into the air
+            isGrounded = false; //the box is no longer on the ground
+        }
+        //this is the character controller
+        //this moves the box in the direction of the axis (left or right and up or down)
+        //times Time.deltaTime which allows it to run at a constant frame rate
+        //and times our movement speed so it moves more than 1f each time.
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision) //is called on collision with something
+    {
+        //checks to see if the box is on the ground after a jump
+        if (collision.gameObject.tag == ("Ground") && !isGrounded)
+        {
+            isGrounded = true; //says that the box is on the ground now
+        }
+
+    }
+}
