752bb3df9a0b52a36b1e6d61d226b986fa49648d
kyuu man <kyuuman@kyuus-MacBook-Pro.local>
2020-02-01 18:06:56 -0600

    merging audio to walls

---
+++ RepairBot/Assets/Audio/AudioManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+        {
+            Destroy((gameObject));
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+        StartGameMusic(); // ****Move to the Start of the main scene
+    }
+
+    public void StartGameMusic()
+    {
+        Play("Head");
+        Play("Torso");
+        Play("Arm1");
+        Play("Arm2");
+        Play("Leg1");
+        Play("Leg2");
+    }
+    
+    public void Play(string soundName) // for playing general sounds
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == soundName);
+        s.source.Play();
+    }
+
+    public void Stop(string soundName)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == soundName);
+        s.source.Stop(); 
+    }
+
+    public void AddTheme(string soundName)
+    {
+        StartCoroutine(VolumeIncrease(soundName));
+    }
+
+    public IEnumerator VolumeIncrease(string soundName) // for adding parts of the main theme when picking up parts
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == soundName);
+
+        while (s.source.volume < .8)
+        {
+            s.source.volume += .02f;
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+    
+}
